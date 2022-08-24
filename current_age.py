9f71096734acc59f19fdf71ac8afd43394ca50a2
abed shabahang <abedsh1374@gmail.com>
2022-08-25 02:52:54 +0430

    Update current_age.py

--- current_age.py
+++ current_age.py
@@ -4,13 +4,14 @@
 
 import datetime
 
+now = datetime.datetime.today()
 date_string = str(input("Please enter your date of birth as YYYY/MM/DD: \n"))
 format = '%Y/%m/%d'
 try:
     datetime.datetime.strptime(date_string, format) #Checking the correctness of the date format and calculating the current age
     list_date_string= date_string.split("/")
     year = int(list_date_string.pop(0))
-    age = 2022 - year
+    age =now.year - year
     print(age)
 
 except ValueError:
